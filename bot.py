e80b30c8650ea96f0fce40ea1310c81e87208f7a
Dmitry <voronov-d88@yandex.ru>
2017-12-11 08:17:10 +0000

    Change to game bot

--- bot.py
+++ bot.py
@@ -1,13 +1,22 @@
 import config
 import telebot
+import os
+import time
 
 
 bot = telebot.TeleBot(config.token)
 
 
-@bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+@bot.message_handler(commands=['test'])
+def find_file_ids(message):
+    for file in os.listdir('music/'):
+        if file.split('.')[-1] != 'ogg':
+            continue
+        with open('music/'+file, 'rb') as file_handler:
+            msg = bot.send_voice(message.chat.id, file_handler, None)
+        # А теперь отправим вслед за файлом его file_id
+        bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+        time.sleep(3)
 
 
 if __name__ == '__main__':
